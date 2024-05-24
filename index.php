483697566f5a5598bb0a5d7cbd0cae0f74879b7c
milad memarian <milad.memarian1373@gmail.com>
2024-05-24 21:54:22 +0330

    Create files structure

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Doodle jump</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="main">
+	<canvas id="gameCanvas" width="400" height="600"></canvas>
+	<canvas id="scoreCanvas" width="200" height="600"></canvas>
+</div>
+<script src="js/main.js"></script>
+<script src="js/game.js"></script>
+<script src="js/player.js"></script>
+<script src="js/obstacle.js"></script>
+<script src="js/websocket.js"></script>
+</body>
+</html>
