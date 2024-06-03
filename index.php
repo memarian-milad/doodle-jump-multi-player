fde19cb44032ebb37127c6565d143e9d4eac2d52
milad memarian <milad.memarian1373@gmail.com>
2024-06-04 01:46:18 +0330

    Add a user info form

--- index.php
+++ index.php
@@ -8,7 +8,29 @@
 </head>
 <body>
 <div id="main">
-	<canvas id="gameCanvas" width="400" height="600"></canvas>
+	<div class="overlay" id="form">
+		<div class="form-container">
+			<div class="form-box">
+				<h2 class="form-title">FORM</h2>
+				<div class="form-grid">
+					<label class="form-label" for="name">NAME</label>
+					<input class="form-input" type="text" id="name">
+				</div>
+				<div class="character-grid">
+					<img src="img/characters/character-1.png" class="character-img" data-code="1" style="grid-column:1" alt="">
+					<img src="img/characters/character-2.png" class="character-img" data-code="2" style="grid-column:2" alt="">
+					<img src="img/characters/character-3.png" class="character-img" data-code="3" style="grid-column:3" alt="">
+					<img src="img/characters/character-4.png" class="character-img" data-code="4" style="grid-column:4" alt="">
+					<img src="img/characters/character-5.png" class="character-img" data-code="5" style="grid-column:5" alt="">
+					<input type="hidden" id="character">
+				</div>
+				<div class="button-container">
+					<button id="submit-button" class="submit-button" type="button">SUBMIT</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<canvas id="gameCanvas" class="" width="400" height="600"></canvas>
 	<canvas id="scoreCanvas" width="200" height="600"></canvas>
 </div>
 <script src="js/main.js"></script>
