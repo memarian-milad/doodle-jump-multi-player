d0f9210850c7457f58a14f30b5f8468462a74d03
milad memarian <milad.memarian1373@gmail.com>
2024-06-04 11:42:01 +0330

    Load user info

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 var userName = "";
 var character = "";
 window.addEventListener ( 'DOMContentLoaded' , () => {
+	this.LoadUserInfo ();
 	this.GetUserInfo ();
 } );
 
+
+function LoadUserInfo()
+{
+	if(localStorage.getItem("userName") && localStorage.getItem("userName") != null)
+	{
+		document.getElementById ( "name" ).value = localStorage.getItem("userName");
+	}
+}
 
 function GetUserInfo ()
 {
