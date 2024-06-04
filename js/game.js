7ae8ff9e832741b453a76a752fd1088a8272a887
milad memarian <milad.memarian1373@gmail.com>
2024-06-04 13:30:50 +0330

    Create a game class structure

---
+++ js/game.js
@@ -0,0 +1,28 @@
+class Game{
+	constructor(canvas, scoreCanvas, obstacle, wsManager) {
+		// The canvas section
+		this.canvas = canvas;
+		this.ctx = canvas.getContext('2d');
+		// The game setting section
+		this.scrollOffset = 0;
+		this.isGameOver = false;
+		this.defaultFallSpeed = 4;
+		this.maxFallSpeed = 6;
+		this.increaseFallSpeed = 0.02;
+		this.fallSpeed = this.defaultFallSpeed;
+		// The obstacle section
+		this.obstacle = obstacle;
+		// The web-socket section
+		this.wsManager = wsManager;
+		// The player section
+		this.players = {};
+		this.currentPlayer = new Player(this.canvas, userName);
+		this.players[this.currentPlayer.id] = this.currentPlayer;
+		this.currentPlayerIsOverObsacle = false;
+		// The scores section
+		this.scoreCanvas = scoreCanvas;
+		this.scoreCtx = scoreCanvas.getContext('2d');
+		this.scores = {};
+		this.setupEventListeners();
+	}
+}
